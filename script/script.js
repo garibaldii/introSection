71eb2ef827757926ddef8009189309d2b84bd8b1
Matheus Garibaldi <125389052+garibaldii@users.noreply.github.com>
2023-07-17 14:41:18 -0300

    recipe 2.0

--- script/script.js
+++ script/script.js
@@ -6,8 +6,11 @@ var login_register = document.getElementById("login_register");
 var menu_position = document.getElementById("menu_position");
 var hamburguer_content_device = document.getElementById("hamburguer_content_device");
 var overlay = document.getElementById("overlay");
-
-var hide_menu = document.getElementById("hide_menu");
+var menu_dropdown = document.getElementById("menu_dropdown");
+var menu_dropdown_b = document.getElementById("menu_dropdown_b");
+var arrow = document.getElementById("arrow");
+var arrow2 = document.getElementById("arrow2")
+var menu_actived = false
 
 window.addEventListener('load', responsive);
 window.addEventListener('resize', responsive);
@@ -46,4 +49,63 @@ function btn_hamburguer_close(){
 }
 
 
+function menu_a(){
+    if (menu_actived){
+        menu_open_a()
+    }
+    else{
+        menu_closed_a()
+    }
 
+    menu_actived = !menu_actived    
+}
+
+function menu_open_a(){
+    menu_actived = true
+    menu_dropdown.style.display = 'flex'
+    menu_dropdown.style.flexDirection = 'column'
+    menu_dropdown.style.listStyle = 'none'
+    menu_dropdown.style.padding = '0'
+    menu_dropdown.style.alignItems = 'flex-end  '
+    menu_dropdown.style.borderRadius = '10px'
+    menu_dropdown.style.backgroundColor = 'white'
+    menu_dropdown.style.boxShadow = '2px 2px 35px 2px rgba(0, 0, 0, 0.2)'
+    arrow.src = 'images/icon-arrow-up.svg'
+}
+
+function menu_closed_a(){
+    menu_dropdown.style.display = 'none'
+    arrow.src = 'images/icon-arrow-down.svg'
+}
+
+
+function menu_b(){
+    if (menu_actived){
+        menu_open_b()
+    }
+    else{
+        menu_closed_b()
+    }
+
+    menu_actived = !menu_actived
+}
+
+
+
+
+function menu_open_b(){
+    menu_actived = true
+    menu_dropdown_b.style.display = 'flex'
+    menu_dropdown_b.style.flexDirection = 'column'
+    menu_dropdown_b.style.listStyle = 'none'
+    menu_dropdown_b.style.padding = '6px'
+    menu_dropdown_b.style.borderRadius = '10px'
+    menu_dropdown_b.style.backgroundColor = 'white'
+    menu_dropdown_b.style.boxShadow = '2px 2px 35px 2px rgba(0, 0, 0, 0.2)'
+    arrow2.src = 'images/icon-arrow-up.svg'
+}
+
+function menu_closed_b(){
+    menu_dropdown_b.style.display = 'none'
+    arrow2.src = 'images/icon-arrow-down.svg'
+}
